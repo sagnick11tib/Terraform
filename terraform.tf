410dfe6da5c165e724ec56e9898fa1e7502e6ada
sagnick11tib <sagnick867@gmail.com>
2025-04-28 18:10:50 +0530

    Fix backend configuration in Terraform file to ensure proper state management

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "nicks-state-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "nicks-state-table"
+  }
 }
